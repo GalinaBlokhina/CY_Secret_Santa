1999505fdfd950fe8a8b71a2283f150dc2965d12
BlokhinaGalina <galina-a@yandex.ru>
2023-04-14 14:54:29 +0300

    16.2 Added after hook

--- cypress/e2e/SS_spec_2.cy.js
+++ cypress/e2e/SS_spec_2.cy.js
@@ -14,4 +14,7 @@ cy.visitPages('[href="/randomizer"] > .btn-secondary', 'randomizer')
 it('account page test', () => {
 cy.visitPages('.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account"] > .header-item > .header-item__text', 'account')
 })
+after('sign out', () => {
+cy.get('.base--clickable > .txt--med').click()
+})
 })
