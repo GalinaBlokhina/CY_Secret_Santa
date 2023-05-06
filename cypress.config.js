30d6685e97bfce0dc76b9b0d2618f4c21e60b848
BlokhinaGalina <galina-a@yandex.ru>
2023-05-06 14:49:01 +0300

    Added randomiser test

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,16 @@ module.exports = defineConfig({
   projectId: "2pbpw2",
   e2e: {
     baseUrl: 'https://staging.lpitko.ru',
+    testIsolation: false,
+    pageLoadTimeout: 100000,
+    watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  env:{
+  email: 'galina-a@yandex.ru',
+  password: 'Test1234'
+ }
 });
+
